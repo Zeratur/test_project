20aed310f417b33d646a79ae29b21b4bcc5f5a1e
Zeratur <15602259139@163.com>
2022-11-22 11:06:46 +0800

    Changes

--- dataConnector.py
+++ dataConnector.py
@@ -20,13 +20,20 @@ port = getConfig("config.ini", "mysql", "port")
 user = getConfig("config.ini", "mysql", "user")
 passwd = getConfig("config.ini", "mysql", "password")
 database = getConfig("config.ini", "mysql", "database")
-properties={'user': user, 'password': passwd}
+properties = {'user': user, 'password': passwd}
 
 
-def read(table, spark):
+def read_db(table, spark):
     url = 'jdbc:mariadb://' + host + ':' + port + '/' + database + '?useSSL=false'
     return spark.read.option("driver", "org.mariadb.jdbc.Driver") \
         .jdbc(url=url, table=table, properties=properties)
 
 
-
+def write_xlsx(df, mode, data_address, file):
+    df.write.format("com.crealytics.spark.excel") \
+        .option("dataAddress", "'My Sheet'!B3:C35") \
+        .option("useHeader", "false") \
+        .option("dateFormat", "yyyy-mm-dd hh:mm:ss") \
+        .option("timestampFormat", "yyyy-mm-dd hh:mm:ss") \
+        .mode("append") \
+        .save("/file.xlsx")  # 要输出的HDFS文件路径.
