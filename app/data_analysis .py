fde55812c98a3dbc44698c3c72c58522c4821e4e
Zeratur <15602259139@163.com>
2022-11-24 17:02:47 +0800

    Changes

--- app/data_analysis .py
+++ app/data_analysis .py
@@ -4,4 +4,8 @@ import sys
 HOME_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(HOME_DIR)
 print(sys.path)
-import utils.dataUtils
+from utils import dataUtils
+if __name__ == "__main__":
+    du = dataUtils.DataUtils()
+    df01 = du.read_db("emp_test01")
+    df01.show()
