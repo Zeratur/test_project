4152ae993a4fd853510c4d473bb8551a46b6373a
Zeratur <15602259139@163.com>
2022-11-24 11:58:47 +0800

    Changes

---
+++ READ ME.md
@@ -0,0 +1,7 @@
+This project runs in Windows environment, for data analysis purpose without any ETL steps.
+The project depends on the following environments:
+1. Scala 2.12
+2. Java 8U351
+3. Python 37
+4. Hadoop 2.7
+5. Spark 3.0.3
--- utils/dataUtils.py
+++ utils/dataUtils.py
@@ -11,70 +11,67 @@ from pyspark.sql import SparkSession
 from pyspark.sql import dataframe as df
 
 
-def getConfig(file_name, section, option):
-    """
-    :param file_name: config file name
-    :param section: config section
-    :param option: config option
-    :return: the Minimum config object
-    """
-    conf = configparser.ConfigParser()
-    conf.read(file_name)
-    config = conf.get(section, option)
-    return config
+class dataUtils:
+    def __init__(self):
+        self.spark = SparkSession.builder.getOrCreate()
+        conf = configparser.ConfigParser()
+        conf.read("../config/config.ini")
+        global host, port, user, password, database, properties
+        host = conf.get("mysql", "host")
+        port = conf.get("mysql", "port")
+        user = conf.get("mysql", "user")
+        password = conf.get("mysql", "password")
+        database = conf.get("mysql", "database")
+        properties = {'user': user, 'password': password}
 
+    def read_db(self, table):
+        """
+        :param table: the table name of the target table
+        :return: return nothing
+        """
+        url = 'jdbc:mariadb://' + host + ':' + port + '/' + database + '?useSSL=false'
+        return self.spark.read.option("driver", "org.mariadb.jdbc.Driver") \
+            .jdbc(url=url, table=table, properties=properties)
 
-host = getConfig("../config/config.ini", "mysql", "host")
-port = getConfig("../config/config.ini", "mysql", "port")
-user = getConfig("../config/config.ini", "mysql", "user")
-passwd = getConfig("../config/config.ini", "mysql", "password")
-database = getConfig("../config/config.ini", "mysql", "database")
-properties = {'user': user, 'password': passwd}
+    def write_db(self, df, table, mode):
+        """
+        :param df: the data frame which you are writing in the database
+        :param table: the target table to which you are writing data
+        :param mode: "append" or "overwrite"
+        :return: return nothing
+        """
+        url = 'jdbc:mariadb://' + host + ':' + port + '/' + database + '?useSSL=false'
+        return df.write.mode(mode).option("truncate", "false").option("driver", "org.mariadb.jdbc.Driver") \
+            .jdbc(url=url, table=table, properties=properties)
 
+    def read_xlsx(self, file_name, data_addr) -> df.DataFrame:
+        """
+        :param file_name: The absolute path to the file
+        :param data_addr: The starting cell of the file
+        :return:
+        """
+        df = self.spark.read.format("com.crealytics.spark.excel") \
+            .option("header", "true") \
+            .option("dataAddress", data_addr) \
+            .load(file_name)
+        return df
 
-def read_db(table, spark):
-    """
-    :param table: the table name of the target table
-    :param spark: sparkSession object
-    :return: return nothing
-    """
-    url = 'jdbc:mariadb://' + host + ':' + port + '/' + database + '?useSSL=false'
-    return spark.read.option("driver", "org.mariadb.jdbc.Driver") \
-        .jdbc(url=url, table=table, properties=properties)
+    def write_xlsx(self, df,  mode, data_address, file_name):
+        """
+        :param df: data_frame create by spark session
+        :param mode: "append" or "overwrite" AS STRING
+        :param data_address: "'sheet1'!A1" or "'sheet1'!A1:C35" AS STRING
+        :param file_name: "file_name" AS STRING
+        :return: return nothing
+        """
+        df.write.format("com.crealytics.spark.excel") \
+            .option("header", "true") \
+            .option("dataAddress", data_address) \
+            .option("dateFormat", "yyyy-mm-dd hh:mm:ss") \
+            .option("timestampFormat", "yyyy-mm-dd hh:mm:ss") \
+            .mode(mode) \
+            .save(HOME_DIR + "/output/" + file_name + ".xlsx")
 
-
-def write_db(df, table, mode):
-    """
-    :param df: the data frame which you are writing in the database
-    :param table: the target table to which you are writing data
-    :param mode: "append" or "overwrite"
-    :return: return nothing
-    """
-    url = 'jdbc:mariadb://' + host + ':' + port + '/' + database + '?useSSL=false'
-    return df.write.mode(mode).option("truncate", "false").option("driver", "org.mariadb.jdbc.Driver") \
-        .jdbc(url=url, table=table, properties=properties)
-
-
-def write_xlsx(df, mode, data_address, file_name):
-    """
-    :param df: data_frame create by spark session
-    :param mode: "append" or "overwrite" AS STRING
-    :param data_address: "'sheet1'!A1" or "'sheet1'!A1:C35" AS STRING
-    :param file_name: "file_name" AS STRING
-    :return: return nothing
-    """
-    df.write.format("com.crealytics.spark.excel") \
-        .option("dataAddress", data_address) \
-        .option("header", "true") \
-        .option("dateFormat", "yyyy-mm-dd hh:mm:ss") \
-        .option("timestampFormat", "yyyy-mm-dd hh:mm:ss") \
-        .mode(mode) \
-        .save(HOME_DIR + "/output/" + file_name + ".xlsx")
-
-
-spark = SparkSession.builder \
-    .getOrCreate()
-
-df01 = read_db("emp_test01", spark)
+du=dataUtils()
+df01 = du.read_db("emp_test01")
 df01.show()
-write_xlsx(df01, mode="append", data_address="A1", file_name="test01")
