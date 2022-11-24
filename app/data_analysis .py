3791ec5882dac60b0205d384547c55213004664c
Zeratur <15602259139@163.com>
2022-11-24 17:13:28 +0800

    Changes

--- app/data_analysis .py
+++ app/data_analysis .py
@@ -5,7 +5,11 @@ HOME_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(HOME_DIR)
 print(sys.path)
 from utils import dataUtils
+from pyspark.sql import SparkSession
+spark=SparkSession.builder.getOrCreate()
+
 if __name__ == "__main__":
     du = dataUtils.DataUtils()
     df01 = du.read_db("emp_test01")
-    df01.show()
+    df01.createOrReplaceTempView("tmp_table")
+    spark.sql("SELECT * FROM tmp_table").show()
